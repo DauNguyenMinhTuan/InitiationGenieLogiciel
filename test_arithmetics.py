254116127cac7ebccd0ce3f59ce9d0e2f1a0e587
ke_An <nkalk192002@gmail.com>
2023-01-18 11:23:12 +0100

    add: complete test

--- test_arithmetics.py
+++ test_arithmetics.py
@@ -3,10 +3,22 @@ from unittest import TestCase
 
 class Test(TestCase):
 	def test_lcm(self):
-		self.assertTrue(lcm(0, 0) == 0)
+		self.assertTrue(0 == lcm(0, 0))
+		self.assertTrue(0 == lcm(0, 100))
+		self.assertTrue(0 == lcm(100, 0))
+		self.assertTrue(200 == lcm(200, 200))
+		self.assertTrue(840 == lcm(60, 168))
 
 	def test_lcm_better(self):
-		self.assertTrue(lcm_better(0, 0) == 0)
+		self.assertTrue(0 == lcm_better(0, 0))
+		self.assertTrue(0 == lcm_better(0, 100))
+		self.assertTrue(0 == lcm_better(100, 0))
+		self.assertTrue(200 == lcm_better(200, 200))
+		self.assertTrue(840 == lcm_better(60, 168))
 
 	def test_lcm_faulty(self):
-		self.assertTrue(lcm_faulty(0, 0) == 0)
+		self.assertTrue(0 == lcm_faulty(0, 0))
+		self.assertTrue(0 == lcm_faulty(0, 100))
+		self.assertTrue(0 == lcm_faulty(100, 0))
+		self.assertTrue(200 == lcm_faulty(200, 200))
+		self.assertTrue(840 == lcm_faulty(60, 168))
